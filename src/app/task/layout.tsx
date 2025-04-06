76f5f7ad1f1d67a5c303b454b0115d9d0e4c9671
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-04-06 17:50:20 +0900

    second commit

---
+++ src/app/task/layout.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const TaskLayout = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    return
+    <div className='bg-blue-300 p-4'>
+        <div>タスクレイアウト</div>
+        {children}
+    </div>;
+};
+
+export default TaskLayout;
